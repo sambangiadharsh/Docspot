134e45abd5b9d6c46c9dd18ddfa87be5d128d950
Adarsh Sambangi <adarsh@Adarshs-MacBook-Air.local>
2025-06-28 17:16:12 -0700

    cors commit

--- Project_Files/backend/routes/admin.js
+++ Project_Files/backend/routes/admin.js
@@ -15,7 +15,7 @@ const createTokenAndSetCookie = (user, res) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'None',
     maxAge: 24 * 60 * 60 * 1000 // 1 day
   });
 };
